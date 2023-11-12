6d3fd8818394d359bbe8660b0a063f4ddfe44d37
Manolis Tzanidakis <mtzanidakis@gmail.com>
2023-11-13 00:24:32 +0200

    switch back to home-manager until the import issue is fixed

--- apply-user.sh
+++ apply-user.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 pushd ~/src/nixos-config
-nix build .#homeManagerConfigurations.manolis-$(hostname -s).activationPackage
-./result/activate
-rm -f -- result
+#nix build .#homeManagerConfigurations.manolis-$(hostname -s).activationPackage
+#./result/activate
+#rm -f -- result
+home-manager switch -f ./users/manolis-$(hostname -s)/home.nix
 popd
