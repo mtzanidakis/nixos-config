e9379d51f714bd432383b8b54aa40f6266666036
Manolis Tzanidakis <mtzanidakis@gmail.com>
2023-11-12 16:32:47 +0200

    add tmux colorscheme; split zsh extras

--- users/manolis/home.nix
+++ users/manolis/home.nix
@@ -99,13 +99,28 @@
     historyLimit = 5000;
     mouse = true;
     terminal = "screen-256color";
+    extraConfig = ''
+      set-option -g status-style fg=yellow,bg=black
+      set-window-option -g window-status-style fg=brightblue,bg=default
+      set-window-option -g window-status-current-style fg=brightred,bg=default
+      set-option -g pane-border-style fg=black
+      set-option -g pane-active-border-style fg=brightgreen
+      set-option -g message-style fg=brightred,bg=black
+      set-option -g display-panes-active-colour blue
+      set-option -g display-panes-colour brightred
+      set-window-option -g clock-mode-colour green
+      set-window-option -g window-status-bell-style fg=black,bg=red
+    '';
   };
 
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
     autocd = true;
-    initExtra = "set -o vi\necho -e \"\n$(fortune -s)\n\"";
+    initExtra = ''
+      set -o vi
+      echo -e "\n$(fortune -s)\n"
+    '';
     oh-my-zsh = {
       enable = true;
       theme = "fox";
