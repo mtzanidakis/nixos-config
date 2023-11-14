c3b41b17323d9caccf0bfe6392a2af92ec4d4ab5
Manolis Tzanidakis <mtzanidakis@gmail.com>
2023-11-14 18:03:32 +0200

    add corefonts and ubuntu fonts

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -2,9 +2,11 @@
 
 {
   fonts.packages = with pkgs; [
+    corefonts
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
+    ubuntu_font_family
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 }
