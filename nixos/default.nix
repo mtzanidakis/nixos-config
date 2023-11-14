db22c569ebdc14860094720c121cc7d3fa199c3d
Manolis Tzanidakis <mtzanidakis@gmail.com>
2023-11-14 17:17:22 +0200

    add locale settings

--- nixos/default.nix
+++ nixos/default.nix
@@ -37,6 +37,18 @@
   # timezone
   time.timeZone = "Europe/Athens";
 
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "el_GR.UTF-8";
+      LC_MONETARY = "el_GR.UTF-8";
+      LC_ADDRESS = "el_GR.UTF-8";
+      LC_TELEPHONE = "el_GR.UTF-8";
+      LC_MEASUREMENT = "el_GR.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+    };
+  };
+
   # console font and keyboard layout
   console = {
     font = "Lat2-Terminus16";
