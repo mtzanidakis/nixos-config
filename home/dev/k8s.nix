5cda5927328588cecd9cb3044928f0005055e0fc
Manolis Tzanidakis <mtzanidakis@gmail.com>
2023-11-15 13:39:07 +0200

    gke-gcloud-auth-plugin in google-cloud-sdk

--- home/dev/k8s.nix
+++ home/dev/k8s.nix
@@ -6,7 +6,7 @@
   ];
 
   home.packages = with pkgs; [
-    google-cloud-sdk
+    (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
     helmfile
     kubectl
     kubectx
