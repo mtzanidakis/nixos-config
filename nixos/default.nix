a50b4a64e9dd30aad31a190101b22aa2e55bd059
Manolis Tzanidakis <mtzanidakis@gmail.com>
2023-11-24 17:43:45 +0200

    split zramswap

--- nixos/default.nix
+++ nixos/default.nix
@@ -7,16 +7,6 @@
    ./vim.nix
   ];
 
-  # enable zram swap
-  zramSwap.enable = true;
-
-  boot.kernel.sysctl = {
-    "vm.page-cluster" = 0;
-    "vm.swappiness" = 180;
-    "vm.watermark_boost_factor" = 0;
-    "vm.watermark_scale_factor" = 125;
-  };
-
   nix = {
     # weekly gc
     gc = {
