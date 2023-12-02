7111cf74cf5f6727874f1b5576e23909c1abf4ed
Manolis Tzanidakis <mtzanidakis@gmail.com>
2023-12-02 17:19:09 +0200

    disable usb autosuspend on monstro for bluetooth

--- hosts/monstro/hardware-configuration.nix
+++ hosts/monstro/hardware-configuration.nix
@@ -10,8 +10,9 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelParams = [ "usbcore.autosuspend=-1" ];
 
   boot.extraModprobeConfig = ''
     options btusb enable_autosuspend=n
