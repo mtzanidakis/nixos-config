9709fef62cf592ca3c69899627fb5dade4c2b61d
Manolis Tzanidakis <mtzanidakis@gmail.com>
2023-12-02 11:32:12 +0200

    add dejavu fonts; corefonts substitution; fontconfig

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -1,12 +1,40 @@
 { config, pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    corefonts
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    ubuntu_font_family
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-  ];
+  fonts = {
+    packages = with pkgs; [
+      corefonts
+      dejavu_fonts
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      ubuntu_font_family
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    ];
+
+    fontconfig = {
+      subpixel.rgba = "rgb";
+      hinting.style = "slight";
+      localConf = ''
+        <alias binding="same">
+           <family>Helvetica</family>
+           <accept>
+           <family>Arial</family>
+           </accept>
+         </alias>
+         <alias binding="same">
+           <family>Times</family>
+           <accept>
+           <family>Times New Roman</family>
+           </accept>
+         </alias>
+         <alias binding="same">
+           <family>Courier</family>
+           <accept>
+           <family>Courier New</family>
+           </accept>
+         </alias>
+      '';
+    };
+  };
 }
