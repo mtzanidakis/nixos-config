7a7177e7807bc927ed0313593fe6aa84da7c202e
Manolis Tzanidakis <mtzanidakis@gmail.com>
2023-12-04 12:07:35 +0200

    add zigu host

---
+++ hosts/zigu/configuration.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../../nixos
+    ../../nixos/bluetooth.nix
+    ../../nixos/docker.nix
+    ../../nixos/flatpak.nix
+    ../../nixos/fonts.nix
+    ../../nixos/kde.nix
+    ../../nixos/lwdevhosts.nix
+    ../../nixos/pipewire.nix
+    ../../nixos/printing.nix
+    ../../nixos/steam.nix
+    ../../nixos/systemd-boot.nix
+    ../../nixos/virt-manager.nix
+    ../../nixos/zramswap.nix
+
+    ./hardware-configuration.nix
+  ];
+
+  networking = {
+    hostName = "zigu";
+    networkmanager.enable = true;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
