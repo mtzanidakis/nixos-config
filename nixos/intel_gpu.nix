bc2243cfce712daafd6e0f8841485589f086d924
Manolis Tzanidakis <mtzanidakis@gmail.com>
2024-01-10 21:49:22 +0200

    add intelgpu video drivers on zigu

--- nixos/intel_gpu.nix
+++ nixos/intel_gpu.nix
@@ -4,11 +4,9 @@
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
-      lact
-      libvdpau-va-gl
-      rocm-opencl-icd
-      rocm-opencl-runtime
-      vaapiVdpau
+      intel-media-driver
+      intel-ocl
+      intel-vaapi-driver
     ];
   };
 }
