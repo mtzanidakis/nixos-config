f2c0dab8b32c0590197b92391334bc9d87bc48d5
Manolis Tzanidakis <mtzanidakis@gmail.com>
2024-06-13 23:18:47 +0300

    remove idle timeout

--- hosts/lopi/hardware-configuration.nix
+++ hosts/lopi/hardware-configuration.nix
@@ -33,7 +33,7 @@
       device = "//u410238.your-storagebox.de/backup";
       fsType = "cifs";
       options = let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+        automount_opts = "x-systemd.automount,noauto,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
         in ["${automount_opts},credentials=/root/.config/storagebox-secrets,uid=1000,gid=1000"];
     };
 
