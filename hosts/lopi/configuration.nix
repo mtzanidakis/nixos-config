180940b385ff11b8e46bbcd4fc9dd7ed3f7e4657
Manolis Tzanidakis <mtzanidakis@gmail.com>
2024-06-15 21:40:04 +0300

    fuse allow-other on lopi

--- hosts/lopi/configuration.nix
+++ hosts/lopi/configuration.nix
@@ -42,6 +42,8 @@
     "/var/services"
   ];
 
+  programs.fuse.userAllowOther = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
