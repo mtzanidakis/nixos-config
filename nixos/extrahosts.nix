fe448029c9f326ff9d291287d0dc4d7531ff8080
Manolis Tzanidakis <mtzanidakis@gmail.com>
2024-10-31 12:03:55 +0200

    add client-canary host

--- nixos/extrahosts.nix
+++ nixos/extrahosts.nix
@@ -11,6 +11,7 @@
     127.0.0.1 assets.learnworlds.develop
     127.0.0.1 aurora.learnworlds.develop
     127.0.0.1 client.learnworlds.develop
+    127.0.0.1 client-canary.learnworlds.develop
     127.0.0.1 companion.learnworlds.develop
     127.0.0.1 kafka-ui.learnworlds.develop
     127.0.0.1 profiler.learnworlds.develop
