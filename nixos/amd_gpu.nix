8f511a80d2ba744fb4977f8135f145612a4e9946
Manolis Tzanidakis <mtzanidakis@gmail.com>
2024-11-23 17:25:51 +0200

    add rocm on amd gpu

--- nixos/amd_gpu.nix
+++ nixos/amd_gpu.nix
@@ -3,9 +3,14 @@
 {
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [
+      clinfo
       lact
       libvdpau-va-gl
+      rocmPackages.clr.icd
+      rocmPackages.rocm-runtime
+      rocmPackages.rocminfo
       vaapiVdpau
     ];
   };
