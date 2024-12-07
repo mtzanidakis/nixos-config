86fd33ac194c3f156da39aa443e3743631dc2e3d
Manolis Tzanidakis <mtzanidakis@gmail.com>
2024-12-07 17:06:32 +0200

    decomission monstro

--- flake.nix
+++ flake.nix
@@ -73,21 +73,6 @@
         ];
       };
 
-      monstro = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/monstro/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.manolis = import ./hosts/monstro/home.nix;
-            };
-          }
-        ];
-      };
-
       paku = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
