e87a98a8189a2cc17cec768e51bcc4914f31d818
Manolis Tzanidakis <mtzanidakis@gmail.com>
2025-01-14 21:42:49 +0200

    switch gizu to amd microcode

--- hosts/gizu/hardware-configuration.nix
+++ hosts/gizu/hardware-configuration.nix
@@ -52,5 +52,5 @@
   # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
