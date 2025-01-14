74ada72d83e4df0cefe8bb1381ef65df1df214dc
Manolis Tzanidakis <mtzanidakis@gmail.com>
2025-01-14 18:32:57 +0200

    enable scrutiny on etsi

--- hosts/etsi/configuration.nix
+++ hosts/etsi/configuration.nix
@@ -69,6 +69,10 @@
     enable = true;
   };
 
+  services.scrutiny = {
+    enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
