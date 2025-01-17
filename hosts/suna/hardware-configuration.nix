e8d83f5f877f4114d812ae8678194a0472068c50
Manolis Tzanidakis <mtzanidakis@gmail.com>
2025-01-17 18:30:14 +0200

    suna: drop nvme & sdmc

--- hosts/suna/hardware-configuration.nix
+++ hosts/suna/hardware-configuration.nix
@@ -11,7 +11,7 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
