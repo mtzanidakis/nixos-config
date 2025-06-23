f52d2e776eeeb4cd2d218779e36e35339477947c
Manolis Tzanidakis <mtzanidakis@gmail.com>
2025-06-23 16:47:31 +0300

    drop go 1.23 dependency for docker build

--- nixos/docker.nix
+++ nixos/docker.nix
@@ -3,9 +3,6 @@
     enable = true;
     autoPrune.enable = true;
     liveRestore = false;
-    package = pkgs.docker.override {
-      buildGoModule = pkgs.buildGo123Module;
-    };
   };
 
   users.users.manolis.extraGroups = ["docker"];
