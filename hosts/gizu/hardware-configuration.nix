9b7a083db64d6e622cdfb3d91dfb434f63817bfd
Manolis Tzanidakis <mtzanidakis@gmail.com>
2026-03-05 15:19:57 +0200

    fix: add timeouts and _netdev for nfs automount on gizu

--- hosts/gizu/hardware-configuration.nix
+++ hosts/gizu/hardware-configuration.nix
@@ -63,7 +63,7 @@
   fileSystems."/mnt/etsi" = {
     device = "192.168.68.65:/storage";
     fsType = "nfs";
-    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=60"];
+    options = ["x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" "x-systemd.device-timeout=5s" "x-systemd.mount-timeout=5s" "_netdev"];
   };
 
   swapDevices = [];
