287829a09d96e7acbdbf8a7a5983bff9a4f7ac36
Manolis Tzanidakis <mtzanidakis@gmail.com>
2026-05-17 20:36:50 +0300

    feat(bluetooth): enable experimental (for battery show) and fastconnect

--- nixos/bluetooth.nix
+++ nixos/bluetooth.nix
@@ -1,4 +1,17 @@
 {
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+    };
+  };
 }
