c51ea34777301f36cc343e52759486c5c86bf368
Manolis Tzanidakis <mtzanidakis@gmail.com>
2026-05-23 01:19:47 +0300

    chore: remove antigravity,codex,cursor,fabric,gemini-cli,lmstudio,warp

--- home/dev/ai.nix
+++ home/dev/ai.nix
@@ -1,15 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    antigravity
     claude-code
-    codex
-    code-cursor-fhs
-    cursor-cli
-    fabric-ai
-    gemini-cli
     llmfit
-    lmstudio
-    warp-terminal
     openai-whisper
     opencode
   ];
