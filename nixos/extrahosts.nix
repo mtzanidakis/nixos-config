ef7cae463bb0a0433d671caf3ef8c674e2729899
Manolis Tzanidakis <mtzanidakis@gmail.com>
2026-06-05 08:42:24 +0300

    chore: drop lw hosts entries

--- nixos/extrahosts.nix
+++ nixos/extrahosts.nix
@@ -1,24 +1,5 @@
 {
   networking.extraHosts = ''
-
-    # lw devsetup hosts
-    127.0.0.1 api.learnworlds.develop
-    127.0.0.1 apitasks-scheduler.learnworlds.develop
-    127.0.0.1 account.learnworlds.develop
-    127.0.0.1 adminer.learnworlds.develop
-    127.0.0.1 analytics.learnworlds.develop
-    127.0.0.1 assets.learnworlds.develop
-    127.0.0.1 aurora.learnworlds.develop
-    127.0.0.1 client.learnworlds.develop
-    127.0.0.1 client-canary.learnworlds.develop
-    127.0.0.1 companion.learnworlds.develop
-    127.0.0.1 kafka-ui.learnworlds.develop
-    127.0.0.1 profiler.learnworlds.develop
-    127.0.0.1 string-is.learnworlds.develop
-    127.0.0.1 www.learnworlds.develop
-    127.0.0.1 urlshortener.learnworlds.develop
-    127.0.0.1 mailpit.learnworlds.develop
-
     # local dev hosts
     127.0.0.1 adminer.local
     127.0.0.1 akyriaki.local
