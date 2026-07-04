6592554a4f3267df58a6157c426062cf631f436c
Manolis Tzanidakis <mtzanidakis@gmail.com>
2026-07-05 00:50:04 +0300

    fix: disable ctrl+r in fzf; use only atuin

--- home/core/fzf.nix
+++ home/core/fzf.nix
@@ -2,5 +2,7 @@
   programs.fzf = {
     enable = true;
     defaultCommand = "rg --files --no-ignore-vcs --hidden";
+    # disable ctrl+r; use atuin instead
+    historyWidget.command = "";
   };
 }
