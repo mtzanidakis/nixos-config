502d3d41b0c509da811228d1fa06c6c61ea09a8e
Manolis Tzanidakis <mtzanidakis@gmail.com>
2026-07-19 20:30:12 +0300

    feat: add handbrake again, the build is fixed upstream

--- home/desktop/multimedia.nix
+++ home/desktop/multimedia.nix
@@ -8,10 +8,7 @@
     ffmpeg
     gimp
     glaxnimate
-    # handbrake # BROKEN in nixpkgs: HandBrake 1.11.1 pins ffmpeg 8.0.1 but
-    # nixpkgs ships ffmpeg_8-full 8.1.2, so its contrib patches fail to apply.
-    # Downgrading ffmpeg doesn't work either (8.0.1 won't compile under gcc15 /
-    # current svt-av1). Re-enable once nixpkgs realigns the versions upstream.
+    handbrake
     imagemagick
     inkscape
     jellyfin-desktop
